a6cc3f06856db25b09594b800210d62e7714713e
JM <jeanmarie.jmendiaye@gmail.com>
2019-09-09 00:46:30 +0000

    insertion d'une feuille de style css avec du php pour indiquer simplement où est logé, placé la feuille de style

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- insertion d'une feuille de style css avec du php pour indiquer simplement où est logé, placé la feuille de style -->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri()) ?>">
 	<meta charset="utf-8">
 	<title>Les acteurs</title>
 </head>
