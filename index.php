54374c5a938ce0d514b7698a2799cde630ee8734
JM <jeanmarie.jmendiaye@gmail.com>
2019-09-09 01:40:04 +0000

    insertion des fonctions php pour intégrer le titre et le slogan selon le tableau de bord et les réglages généraux

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 	<!-- insertion d'une feuille de style css avec du php pour indiquer simplement où est logé, placé la feuille de style -->
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 	<meta charset="utf-8">
-	<title>Les acteurs</title>
+	<!--  Insére 2 fonction pour le titre de l'onglet et le slogan présent dans wordpress au niveau des "Réglages-->
+	<title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?> </title>
 </head>
 
 <body>
