9a98f74fa8cafb7d40ee74b0b1b934ab5069b885
JM <jeanmarie.jmendiaye@gmail.com>
2019-09-09 01:16:51 +0000

    correction de

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<!-- insertion d'une feuille de style css avec du php pour indiquer simplement où est logé, placé la feuille de style -->
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri()) ?>">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 	<meta charset="utf-8">
 	<title>Les acteurs</title>
 </head>
