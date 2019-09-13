25f7e056601cb093b3431703b92324fe6a4a5c77
JM <jeanmarie.jmendiaye@gmail.com>
2019-09-13 15:15:14 +0000

    Intégrer autant d'article dans le container global <main> en incérant les balises <article></article>

--- index.php
+++ index.php
@@ -10,15 +10,13 @@
 
 <body>
 	<main>
-	<?php while (have_posts()) : the_post(); ?>
-
-
-	<?php the_post_thumbnail(medium); ?>
-
-	<h1><?php the_title(); ?></h1>
-	<?php the_content();?>
-
-	<?php endwhile; ?>
+		<?php while (have_posts()) : the_post(); ?>
+		<article>
+		<?php the_post_thumbnail(medium); ?>
+		<h1><?php the_title(); ?></h1>
+		<?php the_content();?>
+		</article>
+		<?php endwhile; ?>
 	</main>
 </body>
 </html>
