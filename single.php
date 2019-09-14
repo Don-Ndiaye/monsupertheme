642642c5c6f3db7fdf658a0108e0baec9e9fede9
JM <jeanmarie.jmendiaye@gmail.com>
2019-09-14 09:19:42 +0000

    modification de la page index.php par single.php avec simplification.

---
+++ single.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- insertion d'une feuille de style css avec du php pour indiquer simplement où est logé, placé la feuille de style -->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+	<meta charset="utf-8">
+	<!--  Insére 2 fonction pour le titre de l'onglet et le slogan présent dans wordpress au niveau des "Réglages-->
+	<title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?> </title>
+</head>
+
+<body>
+	<main>
+		<?php while (have_posts()) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>
+			<?php the_content();?>
+		<?php endwhile; ?>
+	</main>
+</body>
+</html>
